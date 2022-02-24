d720137359bdaac9770f004c9e6a68012308dd3c
Andrea <andrea_mitchel2@yahoo.com>
2022-02-24 16:39:56 -0600

    Iniciar el asistente

--- open_academy/models/wizard.py
+++ open_academy/models/wizard.py
@@ -5,6 +5,15 @@ from odoo import models, fields, api
 class Wizard(models.TransientModel):
     _name = 'open_academy.wizard'
 
+    def _default_session(self):
+        return self.env['open_academy.session'].browse(self._context.get('active_id'))
+
     session_id = fields.Many2one('open_academy.session',
-        string="Session", required=True)
+        string="Session", required=True  default=_default_session)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+
+    @api.multi
+    def subscribe(self):
+        self.session_id.attendee_ids |= self.attendee_ids
+        return {}
