e5c5376cdd3faefbaeb1924b6a8ed1b24dc63c54
Andrea <andrea_mitchel2@yahoo.com>
2022-02-24 21:07:37 -0600

    wizard

--- open_academy/models/wizard.py
+++ open_academy/models/wizard.py
@@ -5,15 +5,15 @@ from odoo import models, fields, api
 class Wizard(models.TransientModel):
     _name = 'open_academy.wizard'
 
-    def _default_session(self):
-        return self.env['open_academy.session'].browse(self._context.get('active_id'))
+    def _default_sessions(self):
+        return self.env['open_academy.session'].browse(self._context.get('active_ids'))
 
-    session_id = fields.Many2one('open_academy.session',
-        string="Session", required=True  default=_default_session)
+    session_ids = fields.Many2many('open_academy.session',
+        string="Sessions", required=True, default=_default_sessions)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
-
-    @api.multi
+    
     def subscribe(self):
-        self.session_id.attendee_ids |= self.attendee_ids
+        for session in self.session_ids:
+            session.attendee_ids |= self.attendee_ids
         return {}
